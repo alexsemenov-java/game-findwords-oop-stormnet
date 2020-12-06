c57462c6ddac9adf2cf16aaff8bd97304dd3e93c
alexsemenov-java <asemenov.java@gmail.com>
2020-12-06 15:51:00 +0300

    Added method for input the name

--- appGame/src/Player.java
+++ appGame/src/Player.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Player {
 
     private String name;
 
-    public Player(String name) {
-        this.name = name;
+    public Player() {
+
+        System.out.println("Enter your name, please!");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        name = input;
     }
 
     public String getName() {
