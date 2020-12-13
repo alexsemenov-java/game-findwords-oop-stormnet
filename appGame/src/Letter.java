89e498ad171ec82af56e26f43f30bf5452e9c814
alexsemenov-java <asemenov.java@gmail.com>
2020-12-13 19:43:36 +0300

    Added constructor

--- appGame/src/Letter.java
+++ appGame/src/Letter.java
@@ -5,4 +5,10 @@ public class Letter {
     private int row;
 
     private int col;
+
+    public Letter(char s, int r, int c) {
+        this.symbol = s;
+        this.row = r;
+        this.col = c;
+    }
 }
