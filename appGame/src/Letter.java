c1306a0e05a7dce2d75feb4b8d7a0d4bf2cd8eaa
alexsemenov-java <asemenov.java@gmail.com>
2020-12-13 20:50:54 +0300

    Change type to String

--- appGame/src/Letter.java
+++ appGame/src/Letter.java
@@ -1,12 +1,12 @@
 public class Letter {
 
-    private char symbol;
+    private String symbol;
 
     private int row;
 
     private int col;
 
-    public Letter(char s, int r, int c) {
+    public Letter(String s, int r, int c) {
         this.symbol = s;
         this.row = r;
         this.col = c;
